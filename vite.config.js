b6b0c28e5108ce00ec2f58d0af406459126336d8
RoyerLopezFlores <roamloflnouse@gmail.com>
2026-08-28 17:42:19 -0500

    add component ficha educativa regional

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/ficha_educativa_regional/',
+    base: '/',
     plugins: [react()],
     server: {
       proxy: {
